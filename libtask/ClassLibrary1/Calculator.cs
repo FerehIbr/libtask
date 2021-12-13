fd9a3630ef87d1a9eaa58b7a277aae3641270986
ferehibrahimli123@outlook.com <ferehibrahimli123@outlook.com>
2021-12-13 22:29:39 +0400

    calculatorlib

---
+++ libtask/ClassLibrary1/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public int Toplama { get; set; }
+        public int Cixma { get; set; }
+        public int Vurma { get; set; }
+        public int Bolme { get; set; }
+    }
+}
---
+++ libtask/libtask/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibrary1;
+using System;
+
+
+namespace libtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Toplama = 6 + 3;
+            calculator.Cixma = 6 - 3;
+            calculator.Vurma = 6 * 3;
+            calculator.Bolme = 6 / 3;
+
+
+            Console.WriteLine(calculator.Toplama);
+            Console.WriteLine(calculator.Bolme);
+            Console.WriteLine(calculator.Vurma);
+            Console.WriteLine(calculator.Bolme);
+
+        }
+        
+    }
+}
